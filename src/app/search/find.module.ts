0f36d5aef049a1463307b4dcfb554d219fcfacb0
Daniela Gonzalez <danygogo@gmail.com>
2021-09-16 13:20:05 -0600

    First commit

---
+++ src/app/search/find.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FindComponent } from './find/find.component';
+
+
+
+@NgModule({
+  declarations: [
+    FindComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FindModule { }
